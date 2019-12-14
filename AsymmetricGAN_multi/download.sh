e17017f1f1cd42bc1c39a77e46545ee6314b9bdd
Ha0Tang <haotang@pku.edu.cn>
2019-12-14 20:35:38 +0000

    update pretrained models

--- AsymmetricGAN_multi/download.sh
+++ AsymmetricGAN_multi/download.sh
@@ -10,28 +10,7 @@ if [ $FILE == "celeba" ]; then
     unzip $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
-
-elif [ $FILE == 'pretrained-celeba-128x128' ]; then
-
-    # StarGAN trained on CelebA (Black_Hair, Blond_Hair, Brown_Hair, Male, Young), 128x128 resolution
-    URL=https://www.dropbox.com/s/7e966qq0nlxwte4/celeba-128x128-5attrs.zip?dl=0
-    ZIP_FILE=./stargan_celeba_128/models/celeba-128x128-5attrs.zip
-    mkdir -p ./stargan_celeba_128/models/
-    wget -N $URL -O $ZIP_FILE
-    unzip $ZIP_FILE -d ./stargan_celeba_128/models/
-    rm $ZIP_FILE
-
-elif [ $FILE == 'pretrained-celeba-256x256' ]; then
-
-    # StarGAN trained on CelebA (Black_Hair, Blond_Hair, Brown_Hair, Male, Young), 256x256 resolution
-    URL=https://www.dropbox.com/s/zdq6roqf63m0v5f/celeba-256x256-5attrs.zip?dl=0
-    ZIP_FILE=./stargan_celeba_256/models/celeba-256x256-5attrs.zip
-    mkdir -p ./stargan_celeba_256/models/
-    wget -N $URL -O $ZIP_FILE
-    unzip $ZIP_FILE -d ./stargan_celeba_256/models/
-    rm $ZIP_FILE
-
 else
-    echo "Available arguments are celeba, pretrained-celeba-128x128, pretrained-celeba-256x256."
+    echo "Available arguments are celeba."
     exit 1
 fi
