25ab3e26784a7ee0de5bedaf810c738bed916ef6
Ha0Tang <haotang@pku.edu.cn>
2019-12-14 20:25:31 +0000

    update readme

---
+++ AsymmetricGAN_multi/run.sh
@@ -0,0 +1,4 @@
+python main.py --mode test --dataset CelebA --image_size 256 --c_dim 5 --batch_size 1 \
+                 --selected_attrs Black_Hair Blond_Hair Brown_Hair Male Young \
+                 --model_save_dir='stargan_celeba_256/models' \
+                 --result_dir='stargan_celeba_256/results'
